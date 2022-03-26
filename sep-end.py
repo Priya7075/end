7639d1bd02f5c5997a5dd2c3a5be856d400af3f7
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-03-26 18:07:18 +0530

    Add files via upload

---
+++ sep-end.py
@@ -0,0 +1,91 @@
+# print("Hello",end=' '"w") #Hello wHello-wHello,w
+# print("Hello",end='-'"w")
+# print("Hello",end=','"w")
+# print("Hello",end=' '"w") #Hello w
+# print("\nHello",end='-'"w") #Hello-w
+# print("\nHello",end=','"w") #Hello,w
+# print("Hello",end=''"welcome to josh innovations\n") #Hellowelcome to josh innovations
+# print("python programming",end=",") #python programming,From Josh Innovations,Course: Core Python Programming
+# print("From Josh Innovations",end=",")
+# print("Course: Core Python Programming")
+# print("Hello IPL on 26th MARCH","2 Days to go",sep='-') #Hello IPL on 26th MARCH-2 Days to go
+# print("Hello",sep=",""Josh Innovations") #Hello
+# print("Cor","python","programming",sep="*") #Cor*python*programming
+# print("Ameerpet Metro Station",end=".") #Ameerpat Metro Station.Near SAP Street LAne
+# print("Near SAP Street Lane",end="\n") 
+# print("Josh","Innovations",end='.',sep="-") #Josh-Innovations.
+# print("Hello World") #Hello World
+# print('Hello World') #Hello World
+# print("program\
+#  on 10th Dec") #program    on 10th Dec
+# print('Hello''Python') #Hellopython
+# print("Hello", 'python') #Hello python
+# print() #blankline
+# print('')#blankline
+# print("")#blankline
+# print('''Hello    #Hello
+# Welcome to        #Welocme to
+# python programming''')#python programming
+# print('''Hello Welcome to \
+#  python programming''') #Hello Welcome to  python programming
+# print("python",
+# "2nd floor") #python 2nd floor
+# print("Josh"), print("Innovations") #Josh
+# print("Josh"); print("Innovations")#innovations
+                                  
+# print("Hello", end=" ") #Hello python
+# print("python")
+# print("Hello",end=". ") #Hello. python hi
+# print("python",end=" ")
+# print("hi")
+# print("Realme" , "GT",sep="") #RealmeGT
+# print("Realme" , "GT",sep=" ") #Realme GT
+# print("Realme" , "GT",sep=".") #Realme.GT
+# print("Realme" , "GT",sep="-") #Realme=GT
+# print("core", "python",end="" "hello") #core pythonhellocore python hellocore python-3
+# print("core", "python",end=" " "hello")
+# print("core", "python",end="-" '3')
+# print("core","python", sep="-" "hello") 
+# print("core","python",end=" " "hello")
+# print("core","python",end="-" "hello")
+# print("core","python",sep="-",end= "hello") #core-pythonhellocore-python hellocore-python-3
+# print("core","python",sep="-",end= " hello")
+# print("core","python",sep="-",end= '-3')
+# print("core","python",end="hello",sep=" ") #core pythonhellocore python hellocore python-3
+# print("core","python",end=" " "hello",sep=" ")
+# print("core","python",end='-3',sep=" ")
+# print("Hello"*10)# HelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+# print("Hello\n"*10,end=".") 
+# print("hello","world") #hello world
+# print("Hello"  "world") #Helloworld
+# print("Hello"+'-'+"world") #Hello-world
+# print("Hello" "world",'core', 'python',sep="") #Helloworldcorepython
+# print("Hello" "world",'core', 'python',sep="-") #Helloworld=core-python
+# print("Hello", "world",'core', 'python',sep="-",) #Hell-=world=core-python
+# print("Hello", "world",'core', 'python',sep="-""ok") #Hello-okworld-okcore-okpython
+# print("Hello", "world",'core', 'python',sep="-"+ "ok")#Hello-okworld-okcore-okpython
+# print("Hello", "world",'core', 'python',sep="-" ' ' "ok")#Hello- okworld- okcore- okpython
+# print("Hello", "python",end=" ") #Hello python Hello python-New Version is 3.10.3
+# print("Hello", "python",end="-") 
+# print("New Version is 3.10.3")
+# print("ok fine") #ok fine
+# print("Hello" , "python",end=".") #Hello python.New Version is 3.10.3 ok fine
+# print("New version is 3.10.3",end=" ")
+# print("ok fine")
+# print("Happy" , "ugadi" , end="." , sep='') #Happyugadi.Happy-ugadi.core@programming@python#
+# print("Happy" , "ugadi" , end="." , sep='-')
+# print("core" , "programming" , "python",sep="@", end="#")
+# print("python\n" *5,end="")
+print("hello" , "world", sep=" 123 ") #hello 123 world
+
+
+
+
+
+
+
+
+
+                  
+
+
